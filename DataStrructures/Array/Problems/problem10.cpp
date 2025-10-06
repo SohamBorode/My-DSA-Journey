17a0b1c5a6f13881c294e2a9b689181a43703a55
Soham Borode <sohamborode6@gmail.com>
2025-10-06 19:42:32 +0530

    problem10.cpp

--- DataStrructures/Array/Problems/problem10.cpp
+++ DataStrructures/Array/Problems/problem10.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 using namespace std;
 // Move all zeros to the end.
-int main()
-{
-    int arr[] = {1, 2, 0, 4, 0, 5, 0, 6, 7, 8, 0, 2, 0, 4, 2, 0, 4};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int no_of_zero = 0;
-    for (int i : arr)
+
+void moveToEnd(int arr[], int n, int target){
+    int no_of_target = 0;
+    for (int i=0; i<n;i++)
     {
-        if (arr[i] == 0)
-            no_of_zero++;
+        if (arr[i] == target)
+            no_of_target++;
     }
 
-    for (int j = 1; j <= no_of_zero; j++)
+    for (int j = 1; j <= no_of_target; j++)
     {
         for (int i = 0; i < n - 1; i++)
         {
-            if (arr[i] == 0 && arr[i + 1] != 0)
+            if (arr[i] == target && arr[i + 1] != target)
             {
                 int temp = arr[i + 1];
                 arr[i + 1] = arr[i];
@@ -24,7 +22,15 @@ int main()
             }
         }
     }
-    for (int i : arr)
-        cout << i << " ";
+     for (int i = 0; i<n;  i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+int main()
+{
+    int arr[] = {1, 2,1,1,0,4,5,1,0,2,4,0,1,1,1,3,4,1,0, 2, 0, 1, 0, 2, 0, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    moveToEnd(arr, n, 1);
     return 0;
 }
