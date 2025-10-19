0d009f5bd9631a93f93f1da1602239216e245aad
Soham Borode <sohamborode6@gmail.com>
2025-10-19 21:37:34 +0530

    Just fun

--- DataStrructures/Array/Problems/problem18.cpp
+++ DataStrructures/Array/Problems/problem18.cpp
@@ -10,20 +10,6 @@ void leftRotate(int arr[], int sz)
     }
     arr[sz - 1] = temp;
 }
-
-// Left rotate an array by d places
-void leftRotateBy_d(int arr[], int sz, int d = 1)
-{
-    while(d!=0){
-    int temp = arr[0];
-    for (int i = 0; i < sz; i++)
-    {
-        arr[i] = arr[i + 1];
-    }
-    arr[sz - 1] = temp;
-    d--;
-}
-}
 int main()
 {
     int arr1[] = {1, 2, 3, 4, 5};
@@ -31,10 +17,5 @@ int main()
     leftRotate(arr1, n1);
     for (int i = 0; i < n1; i++)
         cout << arr1[i] << " ";
-    cout<<endl;
-    int arr2[] = {1, 2, 3, 4, 5};
-    int n2 = sizeof(arr2) / sizeof(arr2[0]);
-    leftRotateBy_d(arr2,n2, 5);
-    for(int i=0; i<n2 ;i++) cout<<arr2[i]<<" ";
     return 0;
 }
---
+++ DataStrructures/Array/Problems/problem21.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+// Right rotate an array by d places.
+void rightRotateBy_d(int arr[], int sz, int d = 1)
+{
+    while (d != 0)
+    {
+        int temp = arr[sz - 1];
+        for (int i = sz - 1; i >= 0; i--)
+        {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = temp;
+        d--;
+    }
+}
+int main()
+{
+    int arr2[] = {1, 2, 3, 4, 5};
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+    rightRotateBy_d(arr2, n2, 3);
+    for (int i = 0; i < n2; i++)
+        cout << arr2[i] << " ";
+
+    return 0;
+}
